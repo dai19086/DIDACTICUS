c47824bed4943ae27e7ba5ae642beba407e841c0
dai19086 <64464732+dai19086@users.noreply.github.com>
2023-03-08 04:32:38 +0200

    refactoring on the way a scenario is temp stored

---
+++ src/app/scenario.model.ts
@@ -0,0 +1,54 @@
+export class Scenario {
+  title: string;
+  duration: string;
+  eduProg: string;
+  //----------page2
+  goals: string;
+  description: string;
+  sciApproach: string;
+  //--------------page3
+  connections: string;
+  multiApproach: string;
+  toolJusti: string;
+  sources: string;
+  //----------page4
+  method: string;
+  microChanges: string;
+  activityEllaboration: string;
+  classOrg: string;
+  //------------page5
+  consensus: string;
+  difficulties: string;
+  noise: string;
+  //--------------page6
+  evaluation: string;
+  reflection: string;
+
+  
+  constructor(){
+    this.title = '';
+    this.duration = '';
+    this.eduProg = '';
+
+    this.goals = '';
+    this.description = '';
+    this.sciApproach = '';
+
+    this.connections = '';
+    this.multiApproach = '';
+    this.toolJusti = '';
+    this.sources = '';
+
+    this.method = '';
+    this.microChanges = '';
+    this.activityEllaboration = '';
+    this.classOrg = '';
+
+    this.consensus = '';
+    this.difficulties = '';
+    this.noise = '';
+
+    this.evaluation = '';
+    this.reflection = '';
+  }
+}
--- src/app/userScreen/new-scenario/new-scenario.component.ts
+++ src/app/userScreen/new-scenario/new-scenario.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Scenario } from 'src/app/scenario.model';
+
 @Component({
   selector: 'app-new-scenario',
   templateUrl: './new-scenario.component.html',
@@ -25,43 +27,19 @@ export class NewScenarioComponent {
 
     { value: "Νηπιαγωγείο", label: "Νηπιαγωγείο" }
   ];
+
   eduProg : string = '';    //link to educational program
   
   page : number = 0;        //used for dectating the page of the form that is currently displayed
 
   id : number = 0;    //the user's id
-  scenario: string[] =['','','','','','','','','','','','','','','','','','',''];   //the scenario's structure that the user inputed
-  //----------page1
-  //0:title
-  //1:duration
-  //2:eduProg
-  //----------page2
-  //3:goals
-  //4:description
-  //5:sciApproach
-  //--------------page3
-  //6:connections
-  //7:multiApproach
-  //8:toolJusti
-  //9:sources
-  //----------page4
-  //10:method
-  //11:microChanges
-  //12:activityEllaboration
-  //13:classOrg
-  //------------page5
-  //14:consensus
-  //15:difficulties
-  //16:noise
-  //--------------page6
-  //17:evaluation
-  //18:reflection
-
+  scenario: Scenario = new Scenario();
 
   constructor () { }
 
   ngOnInit(): void {
     this.page = 1;
+    this.scenario = new Scenario();
 
 
 
@@ -73,7 +51,7 @@ export class NewScenarioComponent {
 
   //used onChange detection to subsequently change the URL that is displayed to the user once a school grade is chosen
   onSelect() {
-    switch (this.scenario[2]) {
+    switch (this.scenario.eduProg) {
       case "Γ'Λυκείου":
         this.eduProg = 'https://www.youtube.com/';
         break;
